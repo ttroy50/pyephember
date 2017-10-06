52b27a8bfb51d076045c14bdba5e05adf4cbd9e1
Thom Troy <ttroy50@gmail.com>
2017-10-06 20:06:42 +0100

    update to v0.0.2

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='pyephember',
-      version='0.0.1',
+      version='0.0.2',
       description='Python library to work with ember from EPH Controls',
       keywords='eph ember',
       author='Thom Troy',
       author_email='ttroy50@gmail.com',
       license='MIT',
       url='https://github.com/ttroy50/pyephember',
-      download_url='https://github.com/ttroy50/pyephember/archive/0.0.1.tar.gz',
+      download_url='https://github.com/ttroy50/pyephember/archive/0.0.2.tar.gz',
       platforms=["any"],
       packages=find_packages(),
       zip_safe=False,
