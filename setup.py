3ff8738989b85abd1e643a30f6b0b78c699f5b94
Thom Troy <ttroy50@gmail.com>
2017-10-08 20:50:55 +0100

    update to v0.1.0 because of new api

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='pyephember',
-      version='0.0.3',
+      version='0.1.0',
       description='Python library to work with ember from EPH Controls',
       keywords='eph ember',
       author='Thom Troy',
       author_email='ttroy50@gmail.com',
       license='MIT',
       url='https://github.com/ttroy50/pyephember',
-      download_url='https://github.com/ttroy50/pyephember/archive/0.0.3.tar.gz',
+      download_url='https://github.com/ttroy50/pyephember/archive/0.1.0.tar.gz',
       platforms=["any"],
       packages=find_packages(),
       zip_safe=False,
